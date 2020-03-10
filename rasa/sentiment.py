0670629a02cbf2a646ff340420c0d8eded105f76
ricksonre <ricksonre@gmail.com>
2020-03-10 15:13:40 -0700

    Update sentiment.py

--- rasa/sentiment.py
+++ rasa/sentiment.py
@@ -7,25 +7,24 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import os
 
 class SentimentAnalyzer(Component):
-    """A pre-trained sentiment component"""
 
+    #initializes variables and arrays
     name = "sentiment"
     provides = ["entities"]
     requires = []
     defaults = {}
     language_list = ["en"]
 
+    #initializes the super class 
     def __init__(self, component_config=None):
         super(SentimentAnalyzer, self).__init__(component_config)
 
+    #model is already trained unecessary
     def train(self, training_data, cfg, **kwargs):
-        """Not needed, because the the model is pretrained"""
         pass
 
-
-
+    #converts the sentiment and confidence into a format compatible with the rasa format
     def convert_to_rasa(self, value, confidence):
-        """Convert model output into the Rasa NLU compatible output format."""
         if (value == "neg"):
                   print("Sad bot")
 	
@@ -36,20 +35,20 @@ class SentimentAnalyzer(Component):
 
         return entity
 
-
+    #process message to retireve sentiment
     def process(self, message, **kwargs):
-        """Retrieve the text message, pass it to the classifier
-            and append the prediction results to the message class."""
-
+        #initializes sentiment intensity analyzer
         sid = SentimentIntensityAnalyzer()
+        #tries to generate the intensite of the sentiment (low,medium,high)
         res = sid.polarity_scores(message.text)
+        #return the highest sentiment in the message
         key, value = max(res.items(), key=lambda x: x[1])
-
+        #coverts the highest sentiment and how confident it is to rasa style
         entity = self.convert_to_rasa(key, value)
-
+        #set the entities contained on the message
+        #   name or subject contained on the message
         message.set("entities", [entity], add_to_output=True)
 
+    #does nothing as model is already pre-trained
     def persist(self, model_dir,yam):
-        """Pass because a pre-trained model is already persisted"""
-
         pass
