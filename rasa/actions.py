67148e7a38904fb0cecb83e0059643253ccfe547
Kaze <renyuan7777@gmail.com>
2020-04-04 21:25:13 -0700

    Yep, need more training from my part to redub the biases

--- rasa/actions.py
+++ rasa/actions.py
@@ -10,7 +10,70 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import operator
 
+def suggestFlower():
+    f = open("tempData/entities.txt", "r")
+    lineRead = f.readlines()
+    wordTally = {
+            "love":0,
+            "marriage":0,
+            "death":0,
+            "loss":0,
+            "pain":0,
+            "hate":0,
+            "hope":0,
+            "sad":0,
+            "pride":0,
+            "inspiration":0,
+            "glory":0,
+            "beware":0,
+            "truth":0,
+            "humility":0,
+            "indifference":0,
+            "poverty":0,
+            "foolishness":0
+    }
+    for line in lineRead:
+        for word in line.split():
+            #word = str(word)
+            if len(word)>4:
+                if word[-4:] == "/NNS":
+                    if word[:len(word)-5] in wordTally:
+                        wordTally[word[:len(word)-5]] = wordTally[word[:len(word)-5]]+1;
+
+                elif word[-3:] == "/NN":
+                    if word[:len(word)-3] in wordTally:
+                        wordTally[word[:len(word)-3]] = wordTally[word[:len(word)-3]]+1;
+            
+    max = 0
+    keyString = "love"
+    for k, v in wordTally.items():
+        if v> max:
+            max = v
+            keyString = k
+    
+    switch = {
+            "love":"Gardenia",
+            "marriage":"Myrtle",
+            "death":"Cypress",
+            "loss":"Ranunculus",
+            "pain":"Marigold",
+            "hate":"Aconite ",
+            "hope":"Daisy",
+            "sad":"Willow",
+            "pride":"Amaryllis",
+            "inspiration":"Angelica",
+            "glory":"Bay tree",
+            "beware":"Begonia",
+            "truth":"Bittersweet",
+            "humility":"Bluebell",
+            "indifference":"Candytuft",
+            "poverty":"Evergreen",
+            "foolishness":"Columbine"
+    }
+    rec = "The flower I recommend is: "+switch[keyString]
+    return rec
 
 class ActionCalculate(Action):
 
@@ -33,16 +96,16 @@ class ActionDecide(Action):
 	tracker: Tracker, 
 	domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        dispatcher.utter_message(text="Reset Potatoes are recommended!")
+        dispatcher.utter_message(text="I recommend russet potatoes")
         return []
 
 class ActionFlowerRecommendation(Action):
     def name(self) -> Text:
-        return "action_flower_recomendation"
+        return "action_flower_recommendation"
 
     def run(self, dispatcher: CollectingDispatcher, 
-        f = open("tempData/entities.txt", "w")
+        tracker: Tracker, 
+	domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        
-        dispatcher.utter_message(text="Reset Potatoes are recommended!")
+        dispatcher.utter_message(text=suggestFlower())
         return []
